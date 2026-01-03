ab7ed3c240208ea10f0a0a817a21702a387c65e8
syams-savero <muhammadsyamssavero@gmail.com>
2026-01-04 03:02:47 +0700

    last fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import Script from "next/script";
 import "./globals.css";
 import SmoothScroll from "@/components/SmoothScroll";
 
-// Setup Font
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-space",
@@ -14,25 +13,16 @@ const spaceGrotesk = Space_Grotesk({
 export const metadata: Metadata = {
   title: "Porsche Catalog",
   description: "Immersive Automotive Showcase",
-  icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'></text></svg>",
-  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-
-      {/* HANYA BOLEH ADA SATU BODY DI SINI */}
-      <body className={`${spaceGrotesk.variable} antialiased bg-black text-white`}>
-        <SmoothScroll /> {}
-
       <head>
-        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-HPFENST1PB"
           strategy="afterInteractive"
@@ -50,13 +40,10 @@ export default function RootLayout({
           }}
         />
       </head>
-
-      {/* HANYA SATU BODY */}
       <body
         className={`${spaceGrotesk.variable} antialiased bg-black text-white`}
       >
         <SmoothScroll />
- bd125be (add Google analytic)
         {children}
       </body>
     </html>
